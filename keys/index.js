6a6a2e1ed33c4710c238a179965b240b72d97fea
Kolorsik <43007323+Kolorsik@users.noreply.github.com>
2020-06-17 10:53:49 +0300

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI: `mongodb+srv://ExpressApp:go00LJUVsZUBlOVx@cluster0-kukiy.mongodb.net/booksShop`,
+    MONGODB_URI: 'URI_FOR_CONNECT_TO_MONGODB',
     SESSION_SECRET: 'some secret value',
     SENDGRID_API_KEY: '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',
     EMAIL_FROM: 'shop-book@list.ru',
